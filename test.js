94fd27352deadf4a1610cde6330ab3c06e66cf55
Thomas Watson Steen <w@tson.dk>
2016-06-17 16:04:56 -0700

    Node.js v0.10 have a different exit code on invalid module

--- test.js
+++ test.js
@@ -26,7 +26,7 @@ test('tests fail', function (t) {
 test('invalid module', function (t) {
   var p = spawn('./index.js', ['test-all-versions-' + Date.now(), '^1.0.0', 'npm', 'test'])
   p.on('close', function (code) {
-    t.equal(code, 1)
+    t.equal(code, process.version.indexOf('v0.10.') === 0 ? 8 : 1)
     t.end()
   })
   p.stdout.pipe(process.stdout)
